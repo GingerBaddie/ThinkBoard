6e28ff3df83f87afa87b1c2e34b4d58cd5c90939
GingerBaddie <professionalmahito@gmail.com>
2026-07-10 17:38:29 +0530

    loader icon added at homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard';
 import NoteDetailPage from './NoteDetailPage';
 import NotesNotFound from '../components/NotesNotFound';
-
+import { LoaderIcon } from 'lucide-react';
 
 function HomePage() {
     const [isRateLimited, setIsRateLimited] = useState(false);
@@ -46,7 +46,7 @@ function HomePage() {
 
             <div className='max-w-7xl mx-auto p-4 mt-6'>
                 
-                {loading && <div className='text-center text-primary py-10'>Loading Notes...</div>}
+                {loading && <div className="min-h-screen bg-base-200 flex items-center justify-center"><LoaderIcon className="animate-spin size-10" /></div>}
 
                 {notes.length === 0 && !isRateLimited && <NotesNotFound />} 
 
